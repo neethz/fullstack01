0610b044eb89ca88e8126a6a3aab4b8bd6a739ea
jeffi <neethu.bc89@gmail.com>
2023-06-30 18:37:16 +0530

    Play with components

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 import './App.css';
-import { Component } from 'react';
+import AllComponent from './Components/AllComponents'
+
 
 function App() {
   return (
     <div className="App">
       My Todo app
-      <FirstComponent></FirstComponent>
-      <SecondComponent></SecondComponent>
-      <ThirdComponent></ThirdComponent>
-      <FourthComponent></FourthComponent>
+      <AllComponent />
     </div>
     
   );
 }
 
-function FirstComponent(){
-  return (
-    <div className='FirstComponent'>First component</div>
-  );
-}
-
-function SecondComponent(){
-  return (
-    <div className='SecondComponent'>Second component</div>
-  );
-}
-
-class ThirdComponent extends Component {
-  render(){
-    return (
-    <div className='ThirdComponent'>Third component</div>
-    );
-  };
-}
-
-class FourthComponent extends Component {
-  render(){
-    return (
-    <div className='FourthComponent'>Fourth Component</div>
-    );
-  };
-}
 
 export default App;
