0c545fdc4d67bc533cfb4ac0e926ac505c68d2e0
jeffi <neethu.bc89@gmail.com>
2023-06-30 13:45:18 +0530

    function and class components use

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
       My Todo app
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <FourthComponent></FourthComponent>
     </div>
+    
   );
+}
+
+function FirstComponent(){
+  return (
+    <div>First component</div>
+  );
+}
+
+function SecondComponent(){
+  return (
+    <div>Second component</div>
+  );
+}
+
+class ThirdComponent extends Component {
+  render(){
+    return (
+    <div>Third component</div>
+    );
+  };
+}
+
+class FourthComponent extends Component {
+  render(){
+    return (
+    <div>Fourth Component</div>
+    );
+  };
 }
 
 export default App;
