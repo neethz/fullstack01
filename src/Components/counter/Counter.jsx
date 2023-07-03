15dd30e285385cf2108fe3903f99c39a7dc37e27
jeffi <neethu.bc89@gmail.com>
2023-07-03 12:20:54 +0530

    use state usage

--- src/Components/counter/Counter.jsx
+++ src/Components/counter/Counter.jsx
@@ -1,16 +1,27 @@
 
+import { useState } from 'react';
 import './Counter.css'
 export default function Counter(){
 
+    const [count, setCount] = useState(0);
+
     function incrementValue(){
-        console.log("Value has been incremented");
+        setCount(count+1);
 
     }
+
+    function reset(){
+        setCount(0);
+    }
+    
     return (
         <div className="counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
                 <button className="increment" onClick={incrementValue}>+1</button>
+            </div>
+            <div>
+            <button className='reset' onClick={reset}>Reset</button>
             </div>
         </div>
     )
