77d55e618c85ae07c27aa7f257b9d1a65f51e8a5
jeffi <neethu.bc89@gmail.com>
2023-07-03 19:30:25 +0530

    conditional show of success and error using js

--- src/Components/todo/TodoApp.jsx
+++ src/Components/todo/TodoApp.jsx
@@ -15,6 +15,17 @@ function Login(){
     const [userName, setUserName] = useState("Neethu");
     const [password, setPassword] = useState("");
 
+    function handleSubmit(){
+        if(userName==='Neethu' && password==='1'){
+            document.querySelector('.AuthSuccess').style.display = 'block';
+            document.querySelector('.AuthFailure').style.display='none';
+
+        }else{
+            document.querySelector('.AuthSuccess').style.display = 'none';
+            document.querySelector('.AuthFailure').style.display='block';
+        }
+    }
+
     return(
         <div className="Login">
             <span name="title">Login to your account</span>
@@ -26,7 +37,9 @@ function Login(){
                 <label>Password : </label>
                 <input type="password" name="password" value={password} onChange={(event) => setPassword(event.target.value)}/>
             </div>
-            <button type="button" name="loginBtn">Login</button>
+            <button type="button" name="loginBtn" onClick={handleSubmit}>Login</button>
+            <div className='AuthSuccess' style={{display:"none"}}>Authentication Successful</div>
+            <div className='AuthFailure' style={{display:"none"}}>Authentication Failed</div>
         </div>
     )
 }
