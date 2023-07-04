a8a24c10abfcac27186cd299c56dfeb04f1e7018
jeffi <neethu.bc89@gmail.com>
2023-07-04 19:06:51 +0530

    authcontext

--- src/Components/todo/TodoApp.jsx
+++ src/Components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import './TodoApp.css'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Error from './Error'
 import Footer from './Footer'
 import Header from './Header'
@@ -7,22 +7,25 @@ import ListTodos from './ListTodos'
 import Login from './Login'
 import Logout from './Logout'
 import Welcome from './Welcome'
+import AuthProvider from './security/AuthContext'
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            <BrowserRouter>
-                <Header />
-                <Routes>
-                    <Route path='/' element={<Login />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/welcome/:username' element={<Welcome />} />
-                    <Route path='/todos' element={<ListTodos />} />
-                    <Route path='/logout' element={<Logout/>}/>
-                    <Route path='*' element={<Error />} />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
+            <AuthProvider>
+                <BrowserRouter>
+                    <Header />
+                    <Routes>
+                        <Route path='/' element={<Login />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/welcome/:username' element={<Welcome />} />
+                        <Route path='/todos' element={<ListTodos />} />
+                        <Route path='/logout' element={<Logout />} />
+                        <Route path='*' element={<Error />} />
+                    </Routes>
+                    <Footer />
+                </BrowserRouter>
+            </AuthProvider>
 
         </div>
     )
--- src/Components/todo/Welcome.jsx
+++ src/Components/todo/Welcome.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react"
 import { useParams, Link } from "react-router-dom"
+import { AuthContext } from "./security/AuthContext"
 
 function Welcome() {
     const { username } = useParams()
+    const authContext = useContext(AuthContext)
+    console.log(authContext.number)
     return (
         <div className='Welcome'>
             <h1>Welcome {username}</h1>
