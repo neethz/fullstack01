90a9e6a67c8eed7084660621fc4750b6ca989040
jeffi <neethu.bc89@gmail.com>
2023-07-05 16:02:49 +0530

    fetch todos from backend

--- src/Components/todo/ListTodos.jsx
+++ src/Components/todo/ListTodos.jsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react"
+import { getUserTodos } from "./api/TodoApiService"
+
 export default function Todos() {
     const today = new Date()
     const targetDate = new Date(today.getFullYear() + 1, today.getMonth(), today.getDay())
-    const todos = [{ id: 1, description: 'Learn AWS', done: false, targetDate: targetDate },
-    { id: 2, description: 'Learn GCP', done: false, targetDate: targetDate },
-    { id: 3, description: 'Learn BE', done: false, targetDate: targetDate }]
+
+
+    const  [todos, setTodos] = useState([])
+    
+    //Below is used to call the function on load without any dependencies. - Fetch data from API when the Todos component is mounted
+    useEffect (
+        () => getTodos() , []
+    )
+   
+
+    function getTodos(){
+        getUserTodos('Neethu')
+        .then((response) => {
+            console.log(response.data)
+            setTodos(response.data)
+
+        })
+        .catch((error) => console.log(error))
+        .finally(() => console.log('Finally todo1'))
+    }
+
     return (
         <div className='container Todos'>
             <h1>Your todos are listed below!</h1>
@@ -23,7 +44,7 @@ export default function Todos() {
                                 <td>{todo.id}</td>
                                 <td>{todo.description}</td>
                                 <td>{todo.done.toString()}</td>
-                                <td>{todo.targetDate.toDateString()}</td>
+                                <td>{todo.targetDate.toString()}</td>
                             </tr>
                         ))
                     }
