a874f1bd75e0d5ea647a59246a94cf9f35bc3f61
jeffi <neethu.bc89@gmail.com>
2023-07-05 12:26:16 +0530

    added axios

--- src/Components/todo/Welcome.jsx
+++ src/Components/todo/Welcome.jsx
@@ -1,3 +1,5 @@
+import axios from "axios"
+import { useState } from "react"
 import { useParams, Link } from "react-router-dom"
 import { useAuth } from "./security/AuthContext"
 
@@ -5,16 +7,33 @@ function Welcome() {
     const { username } = useParams()
     const authContext = useAuth()
 
+
     console.log(authContext.number)
+
+    function helloF(){
+        axios.get("http://localhost:8080/hello-world")
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => console.log(error))
+        .finally(() => console.log('Finally'))
+
+    }
     return (
         <div className='Welcome'>
             <h1>Welcome {username}</h1>
             <div>
                 <Link to='/todos'>My Todos</Link>
             </div>
+            <div>
+               <button className="btn btn-success" onClick={helloF}>Hello World!</button>
+                
+            </div>
 
         </div>
     )
 }
+
+
 
 export default Welcome
