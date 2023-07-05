25fb576ba2b1bf22406a2bb4b1da19801982bf42
jeffi <neethu.bc89@gmail.com>
2023-07-05 12:52:42 +0530

    printing value from object

--- src/Components/todo/Welcome.jsx
+++ src/Components/todo/Welcome.jsx
@@ -6,6 +6,7 @@ import { useAuth } from "./security/AuthContext"
 function Welcome() {
     const { username } = useParams()
     const authContext = useAuth()
+    const [message, setMessage] = useState(null)
 
 
     console.log(authContext.number)
@@ -14,10 +15,21 @@ function Welcome() {
         axios.get("http://localhost:8080/hello-world")
         .then((response) => {
             console.log(response)
+            setMessage(response.data)
         })
         .catch((error) => console.log(error))
         .finally(() => console.log('Finally'))
 
+    }
+
+    function helloB(){
+        axios.get("http://localhost:8080/hello-world-bean")
+        .then ( (response) => {
+            setMessage(response.data.message)
+            console.log(response)
+        })
+        .catch((error) => console.log(error))
+        .finally(() => console.log('Finally v2'))
     }
     return (
         <div className='Welcome'>
@@ -26,9 +38,10 @@ function Welcome() {
                 <Link to='/todos'>My Todos</Link>
             </div>
             <div>
-               <button className="btn btn-success" onClick={helloF}>Hello World!</button>
-                
+               <button className="btn btn-success m-5" onClick={helloF}>Surprise</button>
+               <button className="btn btn-primary m-5" onClick={helloB}>Suprise Bean</button>
             </div>
+            <div className="text text-green">{message}</div>
 
         </div>
     )
