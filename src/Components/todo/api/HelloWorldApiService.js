956b537a210a1f71271069e3cbc88d62aa2d5724
jeffi <neethu.bc89@gmail.com>
2023-07-07 13:30:22 +0530

    basic auth to hello-world api

--- src/Components/todo/api/HelloWorldApiService.js
+++ src/Components/todo/api/HelloWorldApiService.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL : 'http://localhost:8080'
-}) 
+    baseURL: 'http://localhost:8080'
+})
 
-export const retrieveHelloWorld = () => apiClient.get('http://localhost:8080/hello-world') 
-export const retrieveHelloWorldBean = () => apiClient.get('http://localhost:8080/hello-world-bean') 
+export const retrieveHelloWorld = () => apiClient.get('http://localhost:8080/hello-world',
+    {
+        headers: {
+            Authorization: 'Basic TmVldGh1OjE='
+        }
+    })
+export const retrieveHelloWorldBean = () => apiClient.get('http://localhost:8080/hello-world-bean')
 export const retrieveHelloWorldPath = (username) => apiClient.get(`http://localhost:8080/hello-world/path-variable/${username}`)
 
 
